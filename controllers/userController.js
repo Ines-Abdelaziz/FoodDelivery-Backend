af6cd7ccef8f56ad43b8236216401b88eddd310d
MeriemAfafHaddou <jm_haddou@esi.dz>
2023-05-23 06:12:31 +0100

    fixed deployment + added login

---
+++ controllers/userController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+
+module.exports = {
+  register: async (req, res) => {
+    try {
+      const { nomClient, prenomClient, email, mdp, numTlf } = req.body;
+
+      // Check if user already exists
+      const existingUser = await User.findUserByEmail(email);
+      if (existingUser) {
+        return res.status(409).json({ message: 'User already exists' });
+      }
+
+      // Create new user
+      const newUser = await User.createUser({
+        nomClient,
+        prenomClient,
+        email,
+        mdp,
+        numTlf,
+      });
+
+      res.status(201).json({ message: 'Registration successful', user: newUser });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  },
+
+  login: async (req, res) => {
+    try {
+      const { email, mdp } = req.body;
+
+      // Find user by email
+      const user = await User.findUserByEmail(email);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+
+      // Check password
+      if (user.mdp !== mdp) {
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+
+      res.status(200).json({ message: 'Authentication successful', user });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  },
+};
---
+++ models/User.js
@@ -0,0 +1,22 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+module.exports = {
+  createUser: async (userData) => {
+    return await prisma.Client.create({
+      data: {
+        nomClient: userData.nomClient,
+        prenomClient: userData.prenomClient,
+        email: userData.email,
+        mdp: userData.mdp,
+        numTlf: userData.numTlf,
+      },
+    });
+  },
+
+  findUserByEmail: async (email) => {
+    return await prisma.Client.findUnique({
+      where: { email },
+    });
+  },
+};
