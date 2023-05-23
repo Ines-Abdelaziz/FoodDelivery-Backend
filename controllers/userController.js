17b4f5d923d31c4f2efe27c7249a5956a205990e
MeriemAfafHaddou <jm_haddou@esi.dz>
2023-05-23 10:01:08 +0100

    test

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,7 +39,7 @@ module.exports = {
 
       // Check password
       if (user.mdp !== mdp) {
-        return res.status(401).json({ message: 'Invalid credentials' });
+        return res.status(401).json({ message: 'Invalid credentials', email, mdp });
       }
 
       res.status(200).json({ message: 'Authentication successful', user });
