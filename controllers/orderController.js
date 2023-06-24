afa10438737ad84981acb8ef42d73557f4256687
Ines Abdelaziz <66907261+Ines-Abdelaziz@users.noreply.github.com>
2023-06-24 03:07:44 +0100

    fix validate command

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,26 +1,27 @@
 const OrderModel = require('../models/Order');
 module.exports = {
-    validate: async (req, res) => {
-      try {
-        const { idClient, idPerson, prixTotal, address, deliveryNotes, Items, idCommande } = req.body;
-        if(!idClient || !prixTotal || !address || !Items){
-          return res.status(400).json({ error: 'Incomplete command data' });
-        }
-        if (typeof idClient !== 'number' || typeof prixTotal !== 'number' || !Array.isArray(Items)) {
-          return res.status(400).json({ error: 'Invalid command data' });
-        }
-        if (prixTotal <= 0) {
-          return res.status(400).json({ error: 'Invalid total price' });
-        }
-        const orderData=req.body;
-        const createdOrder = await OrderModel.createCommande(orderData);
-        const itemsData = {orderData};
-        const createdItems = await OrderModel.createItems(itemsData);
-          // Return a success response with the created order and items
-          return res.status(200).json({ message: 'Order created successfully', order: createdOrder });
-      } catch (error) {
-        // Handle any errors
-        return res.status(500).json({ error: error.message});
+  validate: async (req, res) => {
+    try {
+      const { idClient, idPerson, prixTotal, address, deliveryNotes, Items, idCommande } = req.body;
+      if(!idClient || !prixTotal || !address || !Items){
+        return res.status(400).json({ error: 'Incomplete command data' });
       }
-    },
+      if (typeof idClient !== 'number' || typeof prixTotal !== 'number' || !Array.isArray(Items)) {
+        return res.status(400).json({ error: 'Invalid command data' });
+      }
+      if (prixTotal <= 0) {
+        return res.status(400).json({ error: 'Invalid total price' });
+      }
+      const orderData=req.body;
+      const createdOrder = await OrderModel.createCommande(orderData);
+      const itemsData = {orderData};
+      const createdItems = await OrderModel.createItems(itemsData);
+      const delivery= await OrderModel.getDelivery(orderData.idPerson)
+        // Return a success response with the created order and items
+        return res.status(200).json(delivery);
+    } catch (error) {
+      // Handle any errors
+      return res.status(500).json({ error: error.message});
+    }
+  },
 };
