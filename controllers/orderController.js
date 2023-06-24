534f62e9bd96fc5fac9a4399205717cfaa71216c
MeriemAfafHaddou <jm_haddou@esi.dz>
2023-06-24 05:15:23 +0100

    push notification

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,4 +1,6 @@
+
 const OrderModel = require('../models/Order');
+const UserModel = require('../models/User');
 module.exports = {
     validate: async (req, res) => {
       try {
@@ -12,11 +14,31 @@ module.exports = {
         if (prixTotal <= 0) {
           return res.status(400).json({ error: 'Invalid total price' });
         }
+
         const orderData=req.body;
         const createdOrder = await OrderModel.createCommande(orderData);
         const itemsData = {orderData};
         const createdItems = await OrderModel.createItems(itemsData);
         const delivery= await OrderModel.getDelivery(orderData.idPerson)
+        const admin = require('firebase-admin');
+
+        // Replace `path/to/serviceAccountKey.json` with the actual path to your downloaded key file
+        const serviceAccount = require('./serviceAccountKey.json');
+
+        admin.initializeApp({
+          credential: admin.credential.cert(serviceAccount),
+        });
+
+        const user=await UserModel.getinfo(idClient);
+        const deviceToken = user.token;
+        console.log(deviceToken)
+        const payload = {
+          notification: {
+            title: 'Preparing Order',
+            body: 'Your order is validated, and it is being prepared ...',
+          },
+        };
+        await admin.messaging().sendToDevice(deviceToken, payload);
           // Return a success response with the created order and items
           return res.status(200).json(delivery);
       } catch (error) {
